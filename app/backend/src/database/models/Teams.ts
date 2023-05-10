9fc5e9d4c9abd89544a492a3e258f6ea626ac8c8
marllonwagner <marllonwagner91@gmail.com>
2023-05-10 16:43:06 -0300

    .

---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,31 @@
+// models/Team.ts
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    teamName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'teams',
+    underscored: true,
+    timestamps: false,
+  },
+);
+
+export default Teams;
