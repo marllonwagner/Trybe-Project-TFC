a6c5e4c240954cae28bec995d36f9dd75d781175
marllonwagner <marllonwagner91@gmail.com>
2023-05-18 13:51:30 -0300

    task 26 /27 /28

--- app/backend/src/leaderboards/leaderBoard.controller.ts
+++ app/backend/src/leaderboards/leaderBoard.controller.ts
@@ -8,6 +8,11 @@ class LeaderBoardController {
     const { statusCode, response } = await this.leaderBoardService.getTeamsResults('homeTeamId');
     return res.status(statusCode).json(response);
   }
+
+  async getAwayTeamsResults(req:Request, res: Response) {
+    const { statusCode, response } = await this.leaderBoardService.getTeamsResults('awayTeamId');
+    return res.status(statusCode).json(response);
+  }
 }
 
 export default LeaderBoardController;
--- app/backend/src/routers/leaderboards.rotuer.ts
+++ app/backend/src/routers/leaderboards.rotuer.ts
@@ -6,5 +6,6 @@ const router = Router();
 const leaderBoardsController = new LeaderBoardController();
 
 router.get('/home', (req, res) => leaderBoardsController.getHomeTeamsResults(req, res));
+router.get('/away', (req, res) => leaderBoardsController.getAwayTeamsResults(req, res));
 
 export default router;
